f2d42dbee7e1f4bc1e0c29075c5d9a9143378a0c
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2018-05-14 20:02:14 +0100

    Attempted own setup with ts again

---
+++ assets/js/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM(<div><h1>Hello</h1></div>, document.getElementById('root'));
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,129 +1,119 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const Md5Hash = require('webpack-md5-hash');
 
-// variables
-var isProduction = process.argv.indexOf('-p') >= 0;
-var sourcePath = path.join(__dirname, './src');
-var outPath = path.join(__dirname, './dist');
-
-// plugins
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+const isProduction = process.argv.indexOf('-p') >= 0;
 
 module.exports = {
-  context: sourcePath,
-  entry: {
-    app: './main.tsx'
-  },
-  output: {
-    path: outPath,
-    filename: 'bundle.js',
-    chunkFilename: '[chunkhash].js',
-    publicPath: '/'
-  },
-  target: 'web',
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
-    // Fix webpack's default behavior to not load packages with jsnext:main module
-    // (jsnext:main directs not usually distributable es6 format, but es6 sources)
-    mainFields: ['module', 'browser', 'main'],
-    alias: {
-      app: path.resolve(__dirname, 'src/app/')
-    }
-  },
-  module: {
-    rules: [
-      // .ts, .tsx
-      {
-        test: /\.tsx?$/,
-        use: isProduction
-          ? 'ts-loader'
-          : ['babel-loader?plugins=react-hot-loader/babel', 'ts-loader']
-      },
-      // css
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              query: {
-                modules: true,
-                sourceMap: !isProduction,
-                importLoaders: 1,
-                localIdentName: '[local]__[hash:base64:5]'
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                ident: 'postcss',
-                plugins: [
-                  require('postcss-import')({ addDependencyTo: webpack }),
-                  require('postcss-url')(),
-                  require('postcss-cssnext')(),
-                  require('postcss-reporter')(),
-                  require('postcss-browser-reporter')({
-                    disabled: isProduction
-                  })
-                ]
-              }
-            }
-          ]
-        })
-      },
-      // static assets
-      { test: /\.html$/, use: 'html-loader' },
-      { test: /\.(png|svg)$/, use: 'url-loader?limit=10000' },
-      { test: /\.(jpg|gif)$/, use: 'file-loader' }
-    ]
-  },
-  optimization: {
-    splitChunks: {
-      name: true,
-      cacheGroups: {
-        commons: {
-          chunks: 'initial',
-          minChunks: 2
-        },
-        vendors: {
-          test: /[\\/]node_modules[\\/]/,
-          chunks: 'all',
-          priority: -10
-        }
-      }
-    },
-    runtimeChunk: true
-  },
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: 'development', // use 'development' unless process.env.NODE_ENV is defined
-      DEBUG: false
-    }),
-    new WebpackCleanupPlugin(),
-    new ExtractTextPlugin({
-      filename: 'styles.css',
-      disable: !isProduction
-    }),
-    new HtmlWebpackPlugin({
-      template: 'assets/index.html'
-    })
-  ],
-  devServer: {
-    contentBase: sourcePath,
-    hot: true,
-    inline: true,
-    historyApiFallback: {
-      disableDotRule: true
-    },
-    stats: 'minimal'
-  },
-  node: {
-    // workaround for webpack-dev-server issue
-    // https://github.com/webpack/webpack-dev-server/issues/60#issuecomment-103411179
-    fs: 'empty',
-    net: 'empty'
-  }
+	mode: isProduction ? 'production' : 'development',
+	entry: {
+		'index.css': [
+			'./sass/index.scss',
+		],
+		'index.js': [
+		  './js/index.tsx',
+		],
+	},
+	output: {
+		path: path.resolve(__dirname, '..', 'public', 'build'),
+		filename: isProduction ? '[chunkhash].[name]' : '[name]',
+		chunkFilename: isProduction ? '[chunkhash].[name]' : '[name]',
+		library: 'blog',
+		libraryTarget: 'umd',
+		umdNamedDefine: true,
+	},
+	resolve: {
+		extensions: ['.js', '.ts', '.tsx'],
+		// Fix webpack's default behavior to not load packages with jsnext:main module
+		// (jsnext:main directs not usually distributable es6 format, but es6 sources)
+		mainFields: ['module', 'browser', 'main'],
+		// alias: {
+		// 	app: path.resolve(__dirname, 'src/app/')
+		// }
+	},
+	target: 'web',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: [
+					{
+						loader: 'ts-loader',
+						//loader: isProduction ? 'ts-loader' : ['babel-loader', 'ts-loader'],
+						options: {
+							allowTsInNodeModules: false,
+							transpileOnly: true
+						},
+					},
+				],
+				exclude: /(node_modules)/,
+			},
+			{
+				test: /.(png|jpg|gif|woff(2)?|eot|ttf|svg|jpg|jpeg)(\?[a-z0-9=\.]+)?$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 10000
+						},
+					},
+				],
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						{
+							loader: 'css-loader',
+						},
+						{
+							loader: 'resolve-url-loader',
+						},
+						{
+							loader: 'sass-loader',
+						},
+					],
+				}),
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						{
+							loader: 'css-loader',
+						},
+						{
+							loader: 'resolve-url-loader',
+						},
+					]
+				}),
+			},
+		],
+	},
+	plugins: [
+		new ExtractTextPlugin({filename: isProduction ? '[chunkhash].[name]' : '[name]', allChunks: false}),
+		new Md5Hash(),
+		new ManifestPlugin({
+			stripSrc: new RegExp('(.css|.js)'),
+		}),
+		new webpack.EnvironmentPlugin({
+			NODE_ENV: 'development',
+			DEBUG: false
+		}),
+		new CleanWebpackPlugin([
+			path.resolve(__dirname, '..', 'public', 'build'),
+		], {
+			root: path.resolve(__dirname, '..'),
+			verbose: true,
+			dry: false,
+			exclude: ['manifest.json'],
+		}),
+		new CompressionPlugin(),
+	],
 };
